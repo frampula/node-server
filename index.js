9ea6709aa9bb57a991f301d569cc292919f67976
framupla <kidalo1337@gmail.com>
2024-04-12 19:30:37 +0300

    Server response

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const http = require('http')
+const http = require("http");
 
 const PORT = 5000;
 
 const requestListener = (request, response) => {
-    const {url, method} = request
-    console.log(url, method)
-}
+  const { url, method } = request;
+  console.log(url, method);
+
+  response.statusCode = 404;
+  response.end('Hello from server');
+};
 
 const server = http.createServer(requestListener);
 
-server.listen(PORT)
+server.listen(PORT);
