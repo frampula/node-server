29cb120e4effa0442d826221b823f14fc03f1103
framupla <kidalo1337@gmail.com>
2024-04-12 18:39:09 +0300

    Remember Common Js

---
+++ index.js
@@ -0,0 +1,3 @@
+const { MyMath, superFunction } = require("./utils/MyMath");
+
+console.log(MyMath.sum(5, 5));
---
+++ utils/MyMath.js
@@ -0,0 +1,21 @@
+class MyMath {
+  static sum = (a, b) => a + b;
+  static sub = (a, b) => a - b;
+  static multy = (a, b) => a * b;
+  static div = (a, b) => a / b;
+}
+
+const object = {
+  key1: "value1",
+  key2: "value2",
+};
+
+const superFunction = () => {
+  return 1 + 2;
+};
+
+module.exports = {
+  MyMath,
+  object,
+  superFunction,
+};
