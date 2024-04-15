bb17ceb8efa0c195a130533372f31919bd0437fd
framupla <kidalo1337@gmail.com>
2024-04-15 19:35:14 +0300

    Yup + BodyParser + Express

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const yup = require("yup");
+const app = express();
+
+const bodyParser = express.json();
+
+const validationSchema = yup.object({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  email: yup.string().required().email(),
+  password: yup.string().required(),
+  isSubscribed: yup.boolean().required(),
+});
 
 const PORT = 5000;
 
-app.get('/', (request, response) => {
-    response.send('Hello world')
-})
-
-app.get('/index.html', (request, response) => {
-  response.status(404).send('Test /index.html')
-})
+app.post("/user", bodyParser, async (req, res, next) => {
+  try {
+    const value = await validationSchema.validate(req.body);
+  console.log(value);
+  } catch (error) {
+    res.status(400).send(error)
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
-})
+});
