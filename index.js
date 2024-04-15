671a07ab614cba1cc483fd3b63e348fe40d2bc84
framupla <kidalo1337@gmail.com>
2024-04-15 18:37:51 +0300

    Express welcome

--- index.js
+++ index.js
@@ -1,51 +1,16 @@
-const http = require("http");
-const fs = require("fs/promises");
+const express = require('express')
+const app = express()
 
 const PORT = 5000;
 
-const requestListener = async (request, response) => {
-  const { url, method } = request;
+app.get('/', (request, response) => {
+    response.send('Hello world')
+})
 
-  if (method === "GET") {
-    if (url === "/index.html") {
-      // http://localhost:5000/index.html
-      try {
-        const data = await fs.readFile("./views/index.html", "utf-8");
-        response.statusCode = 200;
-        response.end(data);
-      } catch (error) {
-        response.statusCode = 404;
-        response.end();
-      }
-    } else if (url === "/style.css") {
-      try {
-        const data = await fs.readFile("./views/style.css", "utf-8"); // http://localhost:5000/style.css
-        response.statusCode = 200;
-        response.end(data);
-      } catch (error) {
-        response.statusCode = 404;
-        response.end();
-      }
-    } else {
-      response.statusCode = 404;
-      response.end();
-    }
-  } else if (method === "POST") {
-    if (url === "/user") {
-      let jsonString = "";
-      request.on("data", (chunk) => {
-        jsonString += chunk;
-      });
-      request.on("end", () => {
-        const user = JSON.parse(jsonString);
-        console.log(user);
+app.get('/index.html', (request, response) => {
+  response.status(404).send('Test /index.html')
+})
 
-        response.statusCode = 200;
-        response.end();
-      });
-    }
-  }
-};
-const server = http.createServer(requestListener);
-
-server.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+})
