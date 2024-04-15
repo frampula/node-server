6c342e37630ec70697fab963132a60dd9e7621e8
framupla <kidalo1337@gmail.com>
2024-04-15 18:14:16 +0300

    server

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 const http = require("http");
+const fs = require("fs/promises");
 
 const PORT = 5000;
 
-const requestListener = (request, response) => {
+const requestListener = async (request, response) => {
   const { url, method } = request;
-  console.log(url, method);
 
-  response.statusCode = 404;
-  response.end('Hello from server');
+  if (method === "GET") {
+    if (url === "/index.html") {
+      // http://localhost:5000/index.html
+      try {
+        const data = await fs.readFile("./views/index.html", "utf-8");
+        response.statusCode = 200;
+        response.end(data);
+      } catch (error) {
+        response.statusCode = 404;
+        response.end();
+      }
+    } else if (url === "/style.css") {
+      try {
+        const data = await fs.readFile("./views/style.css", "utf-8"); // http://localhost:5000/style.css
+        response.statusCode = 200;
+        response.end(data);
+      } catch (error) {
+        response.statusCode = 404;
+        response.end();
+      }
+    } else {
+      response.statusCode = 404;
+      response.end();
+    }
+  } else if (method === "POST") {
+    if (url === "/user") {
+      let jsonString = "";
+      request.on("data", (chunk) => {
+        jsonString += chunk;
+      });
+      request.on("end", () => {
+        const user = JSON.parse(jsonString);
+        console.log(user);
+
+        response.statusCode = 200;
+        response.end();
+      });
+    }
+  }
 };
-
 const server = http.createServer(requestListener);
 
 server.listen(PORT);
